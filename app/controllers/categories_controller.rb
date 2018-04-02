99b6d4fdb061dfa80ac076f3a197e97a65682bac
adam-conway <adam.n.conway@gmail.com>
2018-04-01 20:17:42 -0600

    Added index page for categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,47 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+  #
+  # def create
+  #   @transaction = Transaction.new(transaction_params)
+  #   if @transaction.save
+  #     flash[:success] = "Transaction added!"
+  #     redirect_to transactions_path
+  #   else
+  #     flash[:error] = "Transaction wasn't created successfully"
+  #     @transactions = Transaction.all
+  #     render :index
+  #   end
+  # end
+  #
+  # def edit
+  #   @transaction = Transaction.find(params[:id])
+  # end
+  #
+  # def update
+  #   @transaction = Transaction.find(params[:id])
+  #   @transaction.update(transaction_params)
+  #   if @transaction.save
+  #     flash[:success] = 'Transaction updated!'
+  #     redirect_to transactions_path
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   transaction = Transaction.find(params[:id])
+  #   transaction.destroy
+  #
+  #   flash[:success] = "Transaction from #{transaction.date} successfully deleted!"
+  #   redirect_to transactions_path
+  # end
+  #
+  #
+  # private
+  #
+  # def transaction_params
+  #   params.require(:transaction).permit(:date, :payee, :notes, :inflow, :outflow)
+  # end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401203557) do
+ActiveRecord::Schema.define(version: 20180402021405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.float "current_balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.date "date"
