d37093e51c78ff5a1472ed6b469d3384114c9f69
adam-conway <adam.n.conway@gmail.com>
2018-04-03 13:11:23 -0600

    Added link to create a new budget category from budget show page

--- spec/features/budgets/user_sees_individual_budget_spec.rb
+++ spec/features/budgets/user_sees_individual_budget_spec.rb
@@ -21,31 +21,4 @@ describe "User sees one budget" do
     expect(page).to have_content("Rent")
     expect(page).to have_content("Test")
   end
-  # scenario "a user sees a company contact" do
-  #   company = Company.create!(name: "ESPN")
-  #   contact = company.contacts.create!(name: "Adam", role: "CEO", email: "adam.n.conway@gmail.com")
-  #
-  #   visit company_path(company)
-  #
-  #   expect(page).to have_content("adam.n.conway@gmail.com")
-  #
-  #   click_on("Delete")
-  #
-  #   expect(page).to_not have_content("adam.n.conway@gmail.com")
-  # end
-  # describe "User clicks links" do
-  #   scenario "User clicks jobs link for company" do
-  #     company = Company.create!(name: "ESPN")
-  #     category = Category.create!(title: "Category")
-  #     job1 = company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver", company_id: company.id, category_id: category.id)
-  #     job2 = company.jobs.create!(title: "Writer", level_of_interest: 90, city: "SF", company_id: company.id, category_id: category.id)
-  #     visit company_path(company)
-  #
-  #     click_on("Check out ESPN's jobs!")
-  #
-  #     expect(current_path).to eq(company_jobs_path(company))
-  #     expect(page).to have_content("Developer")
-  #     expect(page).to have_content("Writer")
-  #   end
-  # end
 end
