7fd01282be468bc89ed9906ce8162bdc3aa75bb6
adam-conway <adam.n.conway@gmail.com>
2018-04-03 19:04:57 -0600

    Finished adding authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       redirect_to user_path(user)
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   post '/charge_categories', to: 'chargecategories#create'
 
   resources :users, only: [:new, :create, :show]
