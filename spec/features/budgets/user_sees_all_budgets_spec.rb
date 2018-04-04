810696080ea85294653e29c7cb83b6070134d244
adam-conway <adam.n.conway@gmail.com>
2018-04-04 15:39:03 -0600

    Added better testing to budgets

--- spec/features/budgets/user_sees_all_budgets_spec.rb
+++ spec/features/budgets/user_sees_all_budgets_spec.rb
@@ -12,4 +12,28 @@ describe "User sees all budgets" do
     expect(page).to have_content(budget1.name)
     expect(page).to have_content(budget2.name)
   end
+  scenario "a user navigates to a budget from index" do
+    user = User.create!(username: "Adam", password: "password")
+    budget1 = user.budgets.create!(name: "Denver")
+    budget2 = user.budgets.create!(name: "SF")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_budgets_path(user)
+
+    click_on "Go to Denver budget"
+
+    expect(current_path).to eq(user_budget_path(user, budget1))
+  end
+  scenario "a user navigates to create new budget from index" do
+    user = User.create!(username: "Adam", password: "password")
+    budget1 = user.budgets.create!(name: "Denver")
+    budget2 = user.budgets.create!(name: "SF")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_budgets_path(user)
+
+    click_on "Create a new budget"
+
+    expect(current_path).to eq(new_user_budget_path(user))
+  end
 end
