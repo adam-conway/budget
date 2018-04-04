3290227a9f60ad2cd83efda92fee91764bbc19e3
adam-conway <adam.n.conway@gmail.com>
2018-04-04 13:04:50 -0600

    Finished dynamic nav bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user &.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to root_path
     else
       render :new
     end
