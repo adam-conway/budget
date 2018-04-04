17a7c30f1020f562814c8012a3c9d1e4143b8470
adam-conway <adam.n.conway@gmail.com>
2018-04-04 17:46:08 -0600

    Adding dependent destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   validates :title, :current_balance, presence: true
 
-  has_many :charge_categories
+  has_many :charge_categories, dependent: :destroy
   has_many :charges, through: :charge_categories
   belongs_to :budget
 
